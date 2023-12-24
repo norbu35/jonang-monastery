099cbb72091ae2c96e25b2c6ed5bbf792ab644ee
norbu <93921032+norbu35@users.noreply.github.com>
2023-12-24 15:02:35 +0800

    preact component debug

--- src/components/QuotePreact.tsx
+++ src/components/QuotePreact.tsx
@@ -4,7 +4,9 @@ import type { Quote } from "../../payload-types";
 function QuotePreact(
   { quotes, length }: { quotes: Quote[]; length: number },
 ) {
-  const quote = quotes[Math.floor(Math.random() * length)];
+  const [quote] = useState<Quote | null>(
+    quotes[Math.floor(Math.random() * length)],
+  );
 
   return (
     <figure class="mx-auto my-14 max-w-md px-4 font-quote md:max-w-3xl md:px-10 md:py-16 md:text-xl xl:max-w-4xl">
@@ -14,11 +16,13 @@ function QuotePreact(
             <div
               lang={quote.originalQuoteLang!}
               class="font-tibetan text-lg md:text-xl"
-              dangerouslySetInnerHTML={{ __html: quote.originalQuoteHtml }}
+              dangerouslySetInnerHTML={{
+                __html: quote.originalQuoteHtml,
+              }}
             />
           )
           : null}
-        <div dangerouslySetInnerHTML={{ __html: quote.quoteHtml! }} />
+        <div dangerouslySetInnerHTML={{ __html: quote!.quoteHtml! }} />
       </blockquote>
       <figcaption class="mr-4 mt-12 text-right text-lg md:mt-20 md:text-xl">
         <cite>{quote?.source}</cite>
