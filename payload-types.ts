ea069dc0e8be1f40e3f1fd4861925352ea7c4644
norbu <93921032+norbu35@users.noreply.github.com>
2024-04-21 21:51:41 +0800

    added donate page

--- payload-types.ts
+++ payload-types.ts
@@ -18,8 +18,8 @@ export interface Config {
     cards: Card;
     donations: Donation;
     quotes: Quote;
-    "payload-preferences": PayloadPreference;
-    "payload-migrations": PayloadMigration;
+    'payload-preferences': PayloadPreference;
+    'payload-migrations': PayloadMigration;
   };
   globals: {
     header: Header;
@@ -130,8 +130,8 @@ export interface Page {
         version: number;
         [k: string]: unknown;
       }[];
-      direction: ("ltr" | "rtl") | null;
-      format: "left" | "start" | "center" | "right" | "end" | "justify" | "";
+      direction: ('ltr' | 'rtl') | null;
+      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
       indent: number;
       type: string;
       version: number;
@@ -156,8 +156,8 @@ export interface Newsletter {
         version: number;
         [k: string]: unknown;
       }[];
-      direction: ("ltr" | "rtl") | null;
-      format: "left" | "start" | "center" | "right" | "end" | "justify" | "";
+      direction: ('ltr' | 'rtl') | null;
+      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
       indent: number;
       type: string;
       version: number;
@@ -170,6 +170,7 @@ export interface Newsletter {
 }
 export interface News {
   id: string;
+  slug: string;
   headline: string;
   body: {
     root: {
@@ -178,8 +179,8 @@ export interface News {
         version: number;
         [k: string]: unknown;
       }[];
-      direction: ("ltr" | "rtl") | null;
-      format: "left" | "start" | "center" | "right" | "end" | "justify" | "";
+      direction: ('ltr' | 'rtl') | null;
+      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
       indent: number;
       type: string;
       version: number;
@@ -207,8 +208,8 @@ export interface Teacher {
         version: number;
         [k: string]: unknown;
       }[];
-      direction: ("ltr" | "rtl") | null;
-      format: "left" | "start" | "center" | "right" | "end" | "justify" | "";
+      direction: ('ltr' | 'rtl') | null;
+      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
       indent: number;
       type: string;
       version: number;
@@ -247,8 +248,8 @@ export interface Quote {
         version: number;
         [k: string]: unknown;
       }[];
-      direction: ("ltr" | "rtl") | null;
-      format: "left" | "start" | "center" | "right" | "end" | "justify" | "";
+      direction: ('ltr' | 'rtl') | null;
+      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
       indent: number;
       type: string;
       version: number;
@@ -264,8 +265,8 @@ export interface Quote {
         version: number;
         [k: string]: unknown;
       }[];
-      direction: ("ltr" | "rtl") | null;
-      format: "left" | "start" | "center" | "right" | "end" | "justify" | "";
+      direction: ('ltr' | 'rtl') | null;
+      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
       indent: number;
       type: string;
       version: number;
@@ -281,14 +282,14 @@ export interface Quote {
 export interface PayloadPreference {
   id: string;
   user: {
-    relationTo: "users";
+    relationTo: 'users';
     value: string | User;
   };
   key?: string | null;
   value?:
     | {
-      [k: string]: unknown;
-    }
+        [k: string]: unknown;
+      }
     | unknown[]
     | string
     | number
@@ -307,19 +308,19 @@ export interface PayloadMigration {
 export interface Header {
   id: string;
   navigation: {
-    navigationType?: ("page" | "label" | "link") | null;
+    navigationType?: ('page' | 'label' | 'link') | null;
     page?: (string | null) | Page;
     label?: string | null;
     linkLabel?: string | null;
     url?: string | null;
     sublinks?:
       | {
-        navigationType?: ("page" | "link") | null;
-        page?: (string | null) | Page;
-        linkLabel?: string | null;
-        url?: string | null;
-        id?: string | null;
-      }[]
+          navigationType?: ('page' | 'link') | null;
+          page?: (string | null) | Page;
+          linkLabel?: string | null;
+          url?: string | null;
+          id?: string | null;
+        }[]
       | null;
     id?: string | null;
   }[];
@@ -362,8 +363,8 @@ export interface Activity {
         version: number;
         [k: string]: unknown;
       }[];
-      direction: ("ltr" | "rtl") | null;
-      format: "left" | "start" | "center" | "right" | "end" | "justify" | "";
+      direction: ('ltr' | 'rtl') | null;
+      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
       indent: number;
       type: string;
       version: number;
@@ -384,8 +385,8 @@ export interface Footer {
         version: number;
         [k: string]: unknown;
       }[];
-      direction: ("ltr" | "rtl") | null;
-      format: "left" | "start" | "center" | "right" | "end" | "justify" | "";
+      direction: ('ltr' | 'rtl') | null;
+      format: 'left' | 'start' | 'center' | 'right' | 'end' | 'justify' | '';
       indent: number;
       type: string;
       version: number;
@@ -400,3 +401,7 @@ export interface Footer {
   createdAt?: string | null;
 }
 
+
+declare module 'payload' {
+  export interface GeneratedTypes extends Config {}
+}
